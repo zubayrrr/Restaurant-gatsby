2e2ba0b9dd7114faad24c6e903e09fc2eeac2b42
zubayrrr <abdulfaiz076@gmail.com>
2019-08-23 15:43:02 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Restaurant`,
+    description: `Restaurant website build with Gatsby.`,
+    author: `Zubayr`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
